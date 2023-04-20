bbd23512bdbce91c2e83bfa50f4fca4fcee06c95
Angelo Mendes <51515805+angeloahm@users.noreply.github.com>
2023-04-20 15:51:30 -0300

    Add files via upload

--- Problem Set 5/simulate.m
+++ Problem Set 5/simulate.m
@@ -5,63 +5,105 @@ fprintf('=============================\n')
 fprintf('Simulating the model...\n')
 fprintf('=============================\n')
 
-% Allocate arrays 
-iw_sim = ones(N, T);     %keep track of indices!
-ib_sim = ones(N, T);     %keep track of indices!
-s_sim = ones(N, T);
-u_sim = ones(N, T);    
-%u_sim(1:N/2,T) = 1;
+rng(17)
 
-shock = rand(N,T);                      %generate random numbers
+% Allocate arrays 
+iw_sim = zeros(N, T);       %keep track of indices! (all agents start unemployed: iw=0)
+ib_sim = ones(N, T)*12;        %keep track of indices!
+s_sim = zeros(N, T);
+e_sim = zeros(N, T);    
+
+%generate random numbers
+shock_l = rand(N,T);
+shock_ui = rand(N,T);
+shock_b = rand(N,T);
+shock_p = rand(N,T);
+shock_delta = rand(N,T);
+shock_z = rand(1,T);
+
+% iz_sim(1) = 8;
+% for t=2:T
+%     [~, iz_sim(t)] = max(shock_z(t)<=cumsum(P(iz_sim(t-1),:)));
+% end
 
 mc = dtmc(P);                           %create a markov chain from P
-iz_sim = simulate(mc, T);                %simulate markov chain (indexes)
+iz_sim = simulate(mc, T);               %simulate markov chain (indexes)
 
-for t=1:T-1
-    for n=1:N
-        
-        s_sim(n,t) = policy_effort(iw_sim(n,t), iz_sim(t));
+%Initial states for the benefit are uniformly distributed
+b_pdf=ones(n_w,1)*(1/n_w);
+b_cdf=cumsum(b_pdf);
+
+for n=1:N
+
+    [~,ib_sim(n,1)]=max(shock_b(n,1)<=b_cdf);               %set initial stage for benefits
+    s_sim(n,1) = policy_effort(ib_sim(n,1), iz_sim(1));
+
+    for t=2:T
         
         %Unemployed agent at t
-        if u_sim(n,t) == 1
-            if shock(n,t)<lambda(s_sim(n,t))*p(theta(iw_sim(n,t), iz_sim(t))) %matches a firm and gets a job
-                u_sim(n,t+1) = 0;                                            %becomes employed
-                iw_sim(n,t+1) = idx_wage(iw_sim(n,t), iz_sim(t));             %wage from tomorrow on
-                ib_sim(n,t+1) = ib_sim(n,t+1);                                           
-            else                                                            %does not match a firm
-                u_sim(n,t+1) = 1;   %remains unemployed
-                iw_sim(n,t+1) = iw_sim(n,t+1);
-                ib_sim(n,t+1) = (shock(n,t)<0.1)*1 + (shock(n,t)>0.1)*ib_sim(n,t);    %keep unemployment benefit w.p 0.9
-            end     %end matching case
+        if e_sim(n,t-1) == 0
+           
+            ib_sim(n,t) = (shock_ui(n,t)<=0.1)*1 + (shock_ui(n,t)>0.1)*ib_sim(n,t-1);      %keep unemployment benefit w.p 0.9
+            
+            %matches a firm
+            if shock_l(n,t)<=lambda(s_sim(n,t-1))                                   
+               %Note that the probability of a meeting depends of the
+               %effort made according to (b,z) and just at t+1 I get to
+               %find if I meet or not the firm...
+                
+                %gets a job
+                if shock_p(n,t)<=p( theta( idx_wage(ib_sim(n,t), iz_sim(t)), iz_sim(t) ) )            
+                    e_sim(n,t)  = 1;                                                   %becomes employed
+                    iw_sim(n,t) = idx_wage(ib_sim(n,t), iz_sim(t));                    %wage from tomorrow on
+                    ib_sim(n,t) = 0;
+                    s_sim(n,t) = 0;                                                    %will not search since found a job
+
+                %does not get a job
+                else                                                                
+                    e_sim(n,t) = 0;                                                 %remains unemployed
+                    iw_sim(n,t) = 0;
+                    s_sim(n,t) = policy_effort(ib_sim(n,t), iz_sim(t));             %search according to policy
+                end %end p shock
+
+            %does not match a firm
+            else                                                                  %does not match a firm
+                e_sim(n,t) = 0;                                                   %remains unemployed
+                iw_sim(n,t) = 0;
+                s_sim(n,t) = policy_effort(ib_sim(n,t), iz_sim(t));               %search policy today
+            end     %end lambda shock
+
         %Employed agent at t
         else
-            s_sim(n,t) = policy_effort(iw_sim(n,t), iz_sim(t));
-            if shock(n,t)<delta(iw_sim(n,t), iz_sim(t+1))                     %hit by separation shock
-                u_sim(n,t+1) = 1;   %becomes unemployed
-                iw_sim(n,t+1) = iw_sim(n,t);
-                ib_sim(n,t+1) = (iw_sim(n,t)>1)*floor(iw_sim(n,t)/2) + (iw_sim(n,t)==1);    %first UI is w/2
+            
+            %hit by separation shock
+            if shock_delta(n,t)<=(iz_sim(t)>iw_sim(n,t-1))*delta_bar + (iz_sim(t)<=iw_sim(n,t-1))   
+                e_sim(n,t) = 0;                                                                     %becomes unemployed
+                ib_sim(n,t) = iw_sim(n,t-1);%(iw_sim(n,t-1)>1)*floor(iw_sim(n,t-1)/2) + (iw_sim(n,t-1)==1);%benefit indexed according to wage today!
+                iw_sim(n,t) = 0;               
+                s_sim(n,t) = policy_effort(ib_sim(n,t), iz_sim(t));                                   %employed agent does not exhert any effort
+
+            %does not hit by separation shock
             else
-                u_sim(n,t+1) = 0;           %remains employed
-                iw_sim(n,t+1) = iw_sim(n,t);  %keep same wage
-                ib_sim(n,t+1) = ib_sim(n,t);
+                e_sim(n,t) = 1;               %remains employed 
+                iw_sim(n,t) = iw_sim(n,t-1);  %keep same wage
+                ib_sim(n,t) = 0;
+                s_sim(n,t) = 0;             %employed agent does not exhert any effort
             end     %end separation shock case
         end         %end employment status
 
-        
 
-
-    end     %end loop N
-end         %end loop T
+    end     %end loop T
+end         %end loop N
 
 
 iw_sim = iw_sim(:, burn:T);
 iz_sim = iz_sim(burn:T);
-u_sim = u_sim(:, burn:T);
+e_sim = e_sim(:, burn:T);
 ib_sim = ib_sim(:, burn:T);
 
 % Print statistics
 z_sim=z_grid(iz_sim(:));
-fprintf('Mean unemployment rate:                  %.4f \n', mean(u_sim(:)));
-fprintf('SD unemployment rate/SD productivity:    %.4f \n', std(u_sim(:))/std(z_sim(:)) );
+fprintf('Mean unemployment rate:                  %.4f \n', 1-mean(mean(e_sim)));
+fprintf('SD unemployment rate/SD productivity:    %.4f \n', std((1-e_sim(:)))/std(z_sim(:)) );
 
 
